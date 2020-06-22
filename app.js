4455da15429765a07f6d1bdac2b4f438f727b52c
ImWonYong <ssassb123@daum.net>
2020-06-22 16:28:42 +0900

    ~#2.17

--- app.js
+++ app.js
@@ -7,14 +7,17 @@ import userRouter from "./routers/userRouter";
 import videoRouter from "./routers/userRouter";
 import globalRouter from "./routers/globalRouter";
 import routes from "./routes";
+import { localMiddleware } from "./middlewares";
 
 const app = express();
 
+app.use(helmet());
+app.set("view engine", "pug");
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(morgan("dev"));
-app.use(helmet());
+app.use(localMiddleware);
 
 app.use(routes.home, globalRouter);
 app.use(routes.users, userRouter);
--- controllers/videoController.js
+++ controllers/videoController.js
@@ -1,7 +1,14 @@
-export const home = (req, res) => res.send("Home");
-export const search = (req, res) => res.send("Search");
-export const videos = (req, res) => res.send("Videos");
-export const upload = (req, res) => res.send("Upload");
-export const videoDetail = (req, res) => res.send("Video Detail");
-export const editVideo = (req, res) => res.send("Edit Video");
-export const deleteVideo = (req, res) => res.send("Delete Video");
+export const home = (req, res) =>
+  res.render("home", { pageTitle: "Home", potato: 1234 });
+export const search = (req, res) =>
+  res.render("search", { pageTitle: "Search" });
+export const videos = (req, res) =>
+  res.render("videos", { pageTitle: "Upload" });
+export const upload = (req, res) =>
+  res.render("upload", { pageTitle: "Video Detail" });
+export const videoDetail = (req, res) =>
+  res.render("videoDetail", { pageTitle: "Video Detail" });
+export const editVideo = (req, res) =>
+  res.render("editVideo", { pageTitle: "Edit Video" });
+export const deleteVideo = (req, res) =>
+  res.render("deleteVideo", { pageTitle: "Delete Video" });
