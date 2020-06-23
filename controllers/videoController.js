e720ba466eaa0619505a3a8e121cd82e0063d395
ImWonYong <ssassb123@daum.net>
2020-06-23 17:28:35 +0900

    ~#2.21

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -1,14 +1,24 @@
-export const home = (req, res) =>
-  res.render("home", { pageTitle: "Home", potato: 1234 });
-export const search = (req, res) =>
-  res.render("search", { pageTitle: "Search" });
-export const videos = (req, res) =>
-  res.render("videos", { pageTitle: "Upload" });
+import { videos } from "../db";
+
+export const home = (req, res) => {
+  res.render("home", { pageTitle: "Home", videos });
+};
+
+export const search = (req, res) => {
+  const {
+    query: { term: searchingBy },
+  } = req;
+  res.render("search", { pageTitle: "Search", searchingBy });
+};
+
 export const upload = (req, res) =>
   res.render("upload", { pageTitle: "Video Detail" });
+
 export const videoDetail = (req, res) =>
   res.render("videoDetail", { pageTitle: "Video Detail" });
+
 export const editVideo = (req, res) =>
   res.render("editVideo", { pageTitle: "Edit Video" });
+
 export const deleteVideo = (req, res) =>
   res.render("deleteVideo", { pageTitle: "Delete Video" });
