e937a98d5fa24b5277b76d76008fa42231502a88
ImWonYong <ssassb123@daum.net>
2020-06-24 18:26:23 +0900

    ~#2.24

--- routers/userRouter.js
+++ routers/userRouter.js
@@ -1,7 +1,6 @@
 import express from "express";
 import routes from "../routes";
 import {
-  users,
   userDetail,
   editProfile,
   changePassword,
@@ -11,6 +10,6 @@ const userRouter = express.Router();
 
 userRouter.get(routes.editProfile, editProfile);
 userRouter.get(routes.changePassword, changePassword);
-userRouter.get(routes.userDetail, userDetail);
+userRouter.get(routes.userDetail(), userDetail);
 
 export default userRouter;
