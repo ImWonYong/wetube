5134ab21a6f207f40ada3498a8fabdb8f3fb4867
ImWonYong <ssassb123@daum.net>
2020-07-13 02:00:02 +0900

    ~#6.5

--- app.js
+++ app.js
@@ -3,13 +3,21 @@ import morgan from "morgan";
 import helmet from "helmet";
 import cookieParser from "cookie-parser";
 import bodyParser from "body-parser";
+import passport from "passport";
+import mongoose from "mongoose";
+import session from "express-session";
+import MongoStore from "connect-mongo";
 import userRouter from "./routers/userRouter";
 import videoRouter from "./routers/videoRouter";
 import globalRouter from "./routers/globalRouter";
 import routes from "./routes";
 import { localMiddleware } from "./middlewares";
 
+import "./passport";
+
 const app = express();
+
+const CokieStore = MongoStore(session);
 
 app.use(helmet());
 app.set("view engine", "pug");
@@ -19,6 +27,19 @@ app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(morgan("dev"));
+app.use(
+  session({
+    secret: process.env.COOKIE_SECRET,
+    resave: true,
+    saveUninitialized: false,
+    store: new CokieStore({
+      mongooseConnection: mongoose.connection,
+    }),
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
+
 app.use(localMiddleware);
 
 app.use(routes.home, globalRouter);
