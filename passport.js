70a1468a79522ad06b8a87ccf9dd87a343e760e9
ImWonYong <ssassb123@daum.net>
2020-07-14 16:16:34 +0900

    #~6.12

--- passport.js
+++ passport.js
@@ -26,7 +26,9 @@ passport.use(
     {
       clientID: process.env.FB_ID,
       clientSecret: process.env.FB_SECRET,
-      callbackURL: `http://localhost:4000${routes.facebookCallback}`,
+      callbackURL: `https://ancient-wasp-24.serverless.social${routes.facebookCallback}`,
+      profileFields: ["id", "displayName", "photos", "email"],
+      scope: ["public_profile", "email"],
     },
     facebookLoginCallback
   )
