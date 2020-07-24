068ac76453fad69e3865e6d16fb4c482f5296b64
ImWonYong <ssassb123@daum.net>
2020-07-25 05:14:01 +0900

    fix error

--- src/db.js
+++ src/db.js
@@ -3,11 +3,13 @@ import dotenv from "dotenv";
 dotenv.config();
 
 mongoose.connect(
-  process.env.PRODUCTION ? process.env.MONGO_URL_PROD : process.env.MONGO_URL {
-  useNewUrlParser: true,
-  useFindAndModify: false,
-  useUnifiedTopology: true,
-});
+  process.env.PRODUCTION ? process.env.MONGO_URL_PROD : process.env.MONGO_URL,
+  {
+    useNewUrlParser: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+  }
+);
 
 const db = mongoose.connection;
 
